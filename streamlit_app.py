21dd3159c847d185587b5883a3ff3e097338dd94
Paul Kim <paul.eun.kim@gmail.com>
2024-02-12 10:50:51 -0700

    added heat map and count per company

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,39 +2,45 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import plotly.express as px
+import folium
+from folium.plugins import HeatMap
+from streamlit_folium import st_folium
+# import pandas as pd
 
 """
-# Welcome to Streamlit!
+# Customer Data Analysis
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+This data is from Apollo.io. This is analysis of employees that have the keyword "ADAS" in their titles.
 """
+## plotly adas employees
+combined_df = pd.read_csv('./data/combined.csv')
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+attribute_counts = combined_df['zp_WM8e5'].value_counts().reset_index()
+attribute_counts.columns = ['Attribute', 'Count']
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+fig = px.bar(attribute_counts, x='Attribute', y='Count', 
+             title='',
+             labels={'Attribute': 'Company', 'Count': 'Number of ADAS Employees'})
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+fig.update_layout(xaxis_range=[-0.5, len(attribute_counts['Attribute']) - 0.5], 
+                  yaxis_range=[0, max(attribute_counts['Count']) * 1.1])
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+fig.update_xaxes(tickangle=-45)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+## heat map
+df = pd.read_csv('./data/long_lat_combined.csv')
+df = df.dropna(subset=['Latitude'])
+m = folium.Map(location=[30, -90], zoom_start=4)
+
+heat_data = [[row['Latitude'], row['Longitude']] for index, row in df.iterrows()]
+HeatMap(heat_data, radius=15).add_to(m)
+
+## streamlit display
+with st.container():
+   st.write("Create a bounding box to zoom.")
+   st.plotly_chart(fig)
+
+with st.container():
+    st.write("These are the locations of ADAS employees/companies.")
+    st_data = st_folium(m, width=725)
